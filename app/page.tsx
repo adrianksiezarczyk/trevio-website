3b9fdac31761db81e86910851406f080fdd1407f
adrianksiezarczyk <adrian@takedrop.pl>
2025-10-02 22:32:46 +0200

    init

---
+++ app/page.tsx
@@ -0,0 +1,13 @@
+export default function Home() {
+  return (
+    <div className="font-sans grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20">
+      <main className="flex flex-col gap-[32px] row-start-2 items-center sm:items-start">
+        <ol className="font-mono list-inside list-decimal text-sm/6 text-center sm:text-left">
+          <code className="bg-black/[.05]font-mono font-semibold px-1 py-0.5 rounded text-2xl">
+            Soon.
+          </code>
+        </ol>
+      </main>
+    </div>
+  );
+}
